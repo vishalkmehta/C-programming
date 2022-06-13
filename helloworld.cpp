370be20ee74a61109cbd052541ea09d5633782d4
vishalkmehta <mehtavishal378@gmail.com>
2022-06-13 21:36:33 +0530

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {    
+
+    int i=8, j=10, k;
+    k=i+j; // Addition +
+    printf("The addition of %d and %d is %d \n" ,i,j,k);
+    
+    k=i-j;
+    printf("The Substraction of %d and %d is %d \n" ,i,j,k);
+    
+    k=i*j;
+    printf("The multiplication of %d and %d is %d \n" ,i,j,k);
+    
+    k=i/j;
+    printf("The division of %d and %d is %d \n" ,i,j,k);
+    
+    return 0;
+}
+    
